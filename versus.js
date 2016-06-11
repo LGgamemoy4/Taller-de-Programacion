13026163dd0560a6d08119abf7b77a6e4f449919
LGgamemoy4 <luciano_ledesma2010@yahoo.com.ar>
2016-06-11 18:00:12 +0000

    Juego

---
+++ versus.js
@@ -0,0 +1,12 @@
+var arquero={nombre:"arquero",ataque: 150,vida: 590,VelocidadDeAtaque: 90,velocidad: 60}
+var caballero={nombre:"caballero",ataque: 300,vida: 411,VelocidadDeAtaque: 30,velocidad: 50}
+function versus (jugador1,jugador2) {
+if VelocidadDeAtaque.jugador1>VelocidadDeAtaque.jugador2{
+	jugador2.vida - jugador1.ataque
+}
+else {
+	jugador1.vida - jugador2.ataque
+}
+}
+versus (arquero,caballero);
+console.log (arquero.vida caballero.vida)
